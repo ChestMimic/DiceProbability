7c115eff58a1581b9374ef0b92ac5ceda9f0c9bd
Fitzgibbon <MFitzgibbon@ad.harman.com>
2019-03-15 09:31:17 -0400

    Added function to determine greater than score. Changed less than score to be exclusive LT

--- dice.py
+++ dice.py
@@ -3,6 +3,8 @@ import math
 import logging
 k_0 = 0
 
+#https://www.lucamoroni.it/the-dice-roll-sum-problem/
+
 def binomialCoefficient(top, bottom):
 	#Top chooses Bottom
 	upper = math.factorial(top)
@@ -12,7 +14,6 @@ def binomialCoefficient(top, bottom):
 
 def probabilityScoreOnDice(p, n, s):
 	#Returns the probability of scoring p points on n dice with s sides each
-	#https://www.lucamoroni.it/the-dice-roll-sum-problem/
 	k_max = math.floor((p-n)/s)
 	summation = 0
 	for number in range(k_0, k_max+1):
@@ -22,15 +23,32 @@ def probabilityScoreOnDice(p, n, s):
 	logging.debug("Probability of scoring " + str(p) + " on " + str(n) + "d" + str(s) + " is " + str(returnable))
 	return returnable
 
-def probabilityScoreOrLess(p, n, s):
-	x = 0
+def probabilityLessThan(p, n, s):
+	x = n
 	total = 0
-	while x <= p:
+	while x < p:
 		adding = probabilityScoreOnDice(x,n,s)
 		total = total + adding
 		x = x+1
+	logging.debug("Probability of scoring under " + str(p) + " on " + str(n) + "d" + str(s) + " is " + str(total))
+	return total
+
+def probabilityGreaterThan(p, n, s):
+	x = n*s
+	total = 0
+	while x > p:
+		adding = probabilityScoreOnDice(x,n,s)
+		total = total + adding
+		x = x-1
+	logging.debug("Probability of scoring over " + str(p) + " on " + str(n) + "d" + str(s) + " is " + str(total))
 	return total
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-x = probabilityScoreOrLess(17, 6, 8)
-print(x)
+
+scoreList = []
+scoreList.append(probabilityLessThan(17, 6, 8))
+scoreList.append(probabilityScoreOnDice(17,6,8))
+scoreList.append(probabilityGreaterThan(17,6,8))
+
+print(scoreList)
+print(sum(scoreList))
